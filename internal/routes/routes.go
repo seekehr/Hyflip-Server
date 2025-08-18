cd4db142884dc886c18ec79c815be16441ff25f9
seeker <grouchyseeker@gmail.com>
2025-08-19 04:15:04 +0500

    cors issue

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -9,10 +9,11 @@ import (
 )
 
 func RegisterRoutes(e *echo.Echo, userDb *storage.UserDatabaseClient, hypixelApi *api.HypixelApiClient) {
-	e.Use(middleware.CORSWithConfig(
-		middleware.CORSConfig{
-			AllowOrigins: []string{"*"},
-		}))
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:8080"},
+		AllowMethods: []string{echo.GET, echo.POST, echo.PUT},
+	}))
+
 	e.POST("/create_account", handlers.CreateAccountPostHandler(&handlers.RegisteredPlayers{}, userDb, hypixelApi))
 	protected := e.Group("/api/")
 	protected.Use(handlers.AuthMiddleware(userDb, hypixelApi))
